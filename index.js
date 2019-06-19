b5caa79ef4a4f761441d8b53e02041b8860bf1e7
garvin tan <garvintan07@gmail.com>
2019-06-19 20:47:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,9 +53,9 @@ function removeFromCart(item) {
     if (item === cart[i].itemName){
       cart = cart.splice(i,1)
       return cart
-    
     }
   }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
