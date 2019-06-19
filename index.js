4ddb07c1d500042b455064c0ccadb4b81387a18d
garvin tan <garvintan07@gmail.com>
2019-06-19 19:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ function viewCart() {
   } else if (cart.length===2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else if (cart.length >= 3) {
-    
+     var str = "In your cart, you have "
+     for (i = 0; i < cart.length - 2; i+=1) {
+       str.push(`${cart[i].itemName} at $${cart[i].item{Price})
+       
+     }
   }
     
 }
