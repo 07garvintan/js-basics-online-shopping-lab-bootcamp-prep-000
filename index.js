d6fe3ace4d232c1b749dbd5190c7323c1d611228
garvin tan <garvintan07@gmail.com>
2019-06-19 20:29:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
   for (i=0; i<cart.length;i++){
     prices.push(`${cart[i].itemPrice}`)
   }
-  return prices
+  return prices.map(Number)
 }
 
 function removeFromCart(item) {
