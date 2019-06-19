3bdc5b17e81eeaa842795356f4d955fd1bc58932
garvin tan <garvintan07@gmail.com>
2019-06-19 20:40:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (var i = 0;i<cart.length;i++) {
-    if (item === cart[i]){
+    if (item === cart[i].itemName){
       cart = cart.splice(cart[i])
     }
   }
