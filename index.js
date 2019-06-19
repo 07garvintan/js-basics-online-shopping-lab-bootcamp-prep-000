26f931e6bc9fc6544f02750dd330580f8a739623
garvin tan <garvintan07@gmail.com>
2019-06-19 20:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var str = "In your cart, you have "
-    for (i =0; i < cart.length-1; i+=1)
+    for (i =0; i < cart.length-1; i+=1) {
+      str = str += `${cart[i].itemName} at $${cart[i].itemPrice}`
+    }
   }
   return str
 }
