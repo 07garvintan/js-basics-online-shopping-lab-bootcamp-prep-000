a92650335d13bc4ca6f90479ad19baef85364e98
garvin tan <garvintan07@gmail.com>
2019-06-19 19:44:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function viewCart() {
   // write your code here
   if (cart.length === 0) {
     return "Your shopping cart is empty."
+  } else if (cart.length===1) {
+    return "In your cart, you have "
   }
     
 }
