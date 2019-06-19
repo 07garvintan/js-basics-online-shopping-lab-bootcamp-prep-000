a99e000d4777fdaa599cb261493a4b2687f6feea
garvin tan <garvintan07@gmail.com>
2019-06-19 20:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (typeOfcardNumber === "undefined") {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   }
 }
